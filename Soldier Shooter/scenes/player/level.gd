383dfc5707941a0ab3b8a7d222efa48c1ccddcc2
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-02-19 19:06:05 -0300

    feat: create signals +area

--- Soldier Shooter/scenes/player/level.gd
+++ Soldier Shooter/scenes/player/level.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+var can_laser: bool = true
+var can_grenade: bool = true
+
 func _process(_delta):
 	
 	# input
@@ -7,8 +10,19 @@ func _process(_delta):
 	velocity = direction * 500
 	move_and_slide()
 
-	if Input.is_action_pressed("primary action"):
+	if Input.is_action_pressed("primary action") and can_laser:
 		print("Atirou raio")
+		can_laser = false
+		$Timer.start()
 		
-	if Input.is_action_pressed("secondary action"):
+	if Input.is_action_pressed("secondary action") and can_grenade:
 		print("Atirou granada!")
+		can_grenade = false
+		$GrenadeReloadTimer.start()
+
+func _on_timer_timeout():
+	can_laser = true
+
+
+func _on_grenade_reload_timer_timeout():
+	can_grenade = true
